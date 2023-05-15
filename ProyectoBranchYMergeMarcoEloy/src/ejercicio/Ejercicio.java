a8d4072bb26a130a1fc94545917003f3a1882655
mdominguez <mdominguez@106-31.info2>
2023-05-15 11:54:06 +0200

    Push primero

---
+++ ProyectoBranchYMergeMarcoEloy/src/ejercicio/Ejercicio.java
@@ -0,0 +1,10 @@
+package ejercicio;
+
+public class Ejercicio {
+
+	public static void main(String[] args) {
+	
+	System.out.println("Hola");
+		
+	}
+	}
---
+++ ProyectoBranchYMergeMarcoEloy/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author mdominguez
+ *
+ */
+module ProyectoBranchYMergeMarcoEloy {
+}
